d9df60732e320330740b624a333f1b6b7ca41cb8
Gesdes <50413650+Gesdes@users.noreply.github.com>
2020-10-15 17:28:20 -0500

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,101 @@
+import { Component, OnInit } from '@angular/core';
+import * as Feather from 'feather-icons';
+import { OrganizationService } from '../services/organization.service';
+import { Empresa } from "../models/empresa"
+import { ConvocatoriaServices } from '../services/convocatoria.service';
+import { Convocatoria,Tipo } from "../models/convocatoria"
+
+import { ProyectoService } from '../services/proyecto.service';
+import { AlumnosProyectos } from "../models/proyectos"
+import { SessionService } from '../services/session.service';
+
+declare var $: any;
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  public empresa: Empresa[] = [  ];
+  public empresacantidad: number;
+  public empresaactiva: Empresa[] = [  ];
+  public empresadesaciva: Empresa[] = [  ];
+  public d: Date = new Date(); 
+  public tipoModel = new Tipo(1);
+  public convocatorias:Convocatoria [] = [ ];
+  public convocatoriasalumnos:Convocatoria [] = [ ];
+  public project: AlumnosProyectos[] = [];
+
+  public convocatoriasf:Convocatoria [] = [ ];
+  public convocatoriasalumnosf:Convocatoria [] = [ ];
+
+  public idproyecto:string;
+  public proyecto:string;
+ 
+  constructor( private organizacionService: OrganizationService, private convocatoriaService: ConvocatoriaServices,private proyectoService: ProyectoService,public session: SessionService) { 
+  
+  }
+
+
+  ngOnInit(): void {
+
+
+
+
+       this.convocatorias = [ ];
+     this.convocatoriasalumnos = [ ];
+     this.convocatoriasf = [ ];
+     this.convocatoriasalumnosf = [ ];
+     this.obtenerProyectos();
+
+    this.obtenerConvocatoria2();
+
+
+
+
+  }
+
+  ngAfterViewInit() {
+    Feather.replace();
+
+  }
+
+
+  obtenerConvocatoria2() {
+    let model = this.tipoModel;
+    model.tipo=2;
+    this.convocatoriaService.getConvocatoriatipo(model).subscribe((res: any[])=>{
+this.convocatoriasalumnos=res;
+for(var i=0;i<this.convocatoriasalumnos.length;i++){
+  var fech= Date.parse(this.convocatoriasalumnos[i].fechaTermino.toString());
+
+
+if(fech < Date.now() ){
+
+
+this.convocatoriasalumnosf.push(this.convocatoriasalumnos[i]);
+
+}
+
+}
+console.log(this.convocatoriasalumnosf);
+
+
+
+})
+  }
+
+  obtenerProyectos() {
+    var id=this.session.getToken();
+console.log(id);
+     this.proyectoService.getProyectoalumno(id).subscribe((res: any[])=>{        
+this.idproyecto=res["idProyecto"];
+this.proyecto=res["proyectoNombre"];
+
+});
+}
+
+  
+
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginServices } from '../services/login.service';
+import { SessionService } from '../services/session.service';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+public mensaje="";
+  constructor(public session: SessionService,private router: Router,private loginservice: LoginServices){ }
+
+  ngOnInit(): void {
+    this.session.Signoff();
+
+  }
+
+  onSubmit(data) {
+console.log(data);
+    
+    this.loginservice.login(data.value).subscribe((res: any)=>{
+if(res['resultado']==1){
+  var datosvalue=res['datos'];
+  this.session.setToken(datosvalue['id']);
+ 
+  this.session.setnombre(datosvalue['nombre']);
+  this.session.setapellidos(datosvalue['apellidos']);
+
+    this.router.navigate(['/dashboard']);
+
+}else{
+  this.mensaje=res['mensaje'];
+  $('#success-modal-preview').modal('show');
+
+}
+
+    }, error=>{
+      alert(error.error)
+    })
+    
+    
+
+  
+
+  }
+
+}
